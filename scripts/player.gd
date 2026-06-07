9b14a47214c201f99acaacce3260c31d11badd16
Emilien OLIVAREZ <eolivarez2008@gmail.com>
2026-06-07 21:18:01 +0200

    feat: init project structure and player movement

---
+++ scripts/player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+@export var speed := 200.0
+
+func _physics_process(delta: float) -> void:
+	var direction := Vector2.ZERO
+
+	direction.x = Input.get_axis("ui_left", "ui_right")
+	direction.y = Input.get_axis("ui_up", "ui_down")
+
+	if direction != Vector2.ZERO:
+		direction = direction.normalized()
+
+	velocity = direction * speed
+	move_and_slide()
